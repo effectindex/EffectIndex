ab602d5ad8ee451e3e0dd3aa00dc07b0ff63f77a
Gabriel Morris <gabemorris@outlook.com>
2018-06-21 12:09:40 -0700

    Added Discord-chat page

---
+++ pages/index/discord-chat.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="pageContent">
+        <h1> Discord chat <i class="fa fa-heart-o categoryIcon"> </i> </h1>
+
+        <p> The official Effect Index community is hosted on Discord and may be joined <ext-link href="https://discord.gg/mNFMQpN"> here </ext-link>
+        </p>
+
+        <p> We believe that in pioneering formalised subjective effect documentation we may demystify
+            the psychedelic experience, which has the potential to allow hallucinogen usage to become
+            more culturally acceptable, better understood, and create a platform on top of which these
+            substances may be more easily studied.
+        </p>
+
+        <p>
+            Effect Index was initially founded as a side project on 30 June, 2017 by Josie Kins, the
+            founder of <ext-link href="http://www.psychonautwiki.org"> PsychonautWiki </ext-link> and
+            <ext-link href="http://disregardeverythingisay.com/"> DisregardEverythingISay</ext-link>.
+            It serves as a platform for content
+            that has been in constant development for the previous six years on these sites. However,
+            the aforementioned content is now hosted on its own dedicated platform in the hope of further
+            spreading the documentation and creating a universal terminology set which gives people the
+            vernacular to fully describe experiences that were previously considered ineffable.
+        </p>
+
+        <p>
+            If you find this project interesting, we encourage you to join our large and active community
+            which can be found within both our <ext-link href="http://reddit.com/r/replications"> subreddit </ext-link>
+            and <ext-link href="https://discord.gg/mNFMQpN"> Discord server. </ext-link>
+        </p>
+
+    </div>
+</template>
+
+<script>
+import ExtLink from '@/components/ExtLink';
+
+export default {
+    components: {
+        ExtLink
+    }
+}
+</script>
