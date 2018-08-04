ccf9ddb80807d39d1795719f31b3003a02ac0590
Viscid <viscid@gmail.com>
2018-08-03 21:33:43 -0400

    Oops

--- pages/index/effects/_name/index.vue
+++ pages/index/effects/_name/index.vue
@@ -35,10 +35,11 @@
           <h3> See Also </h3>
           <ul>
             <li
-              v-scroll-to="{el: '#__nuxt' }"
               v-for="link in effect.see_also"
               :key="link.location">
-              <nuxt-link :to="link.location"> {{ link.title }} </nuxt-link>
+              <nuxt-link
+                v-scroll-to="{el: '#__nuxt' }"
+                :to="link.location"> {{ link.title }} </nuxt-link>
             </li>
           </ul>
         </div>
