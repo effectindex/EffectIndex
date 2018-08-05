7a06617dce9c7ab046762a1a0bbe7dc44300a0af
Gabriel Morris <gabemorris@outlook.com>
2018-08-05 12:32:25 -0700

    Updated paypal email

--- pages/index/take-action.vue
+++ pages/index/take-action.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pageContent">
-    
+
     <h1> Donate <i class="fa fa-heart-o categoryIcon" /> </h1>
 
     <p> If you would be interested in contributing to the Effect Index project we have a number of donation options available. The funds will be used for hosting costs and enable us to devote more of our time to Subjective Effect Documentation. Any contribution is greatly appreciated!
@@ -8,9 +8,9 @@
 
     <h3> Options </h3>
     <ul>
-      <li><b>Patreon</b> - <ext-link href="http://patreon.com/effectindex">Effect Index Patreon</ext-link></li>
-      <li><b>Paypal</b> - wtfaregirlylaps@gmail.com</li>
-      <li><b>Bitcoin</b> - 1DvGTSLskP7eYB4t1Mi7KvV56rPEmSg9WT</li>
+      <li> <b>Patreon</b> - <ext-link href="http://patreon.com/effectindex">Effect Index Patreon</ext-link> </li>
+      <li> <b>Paypal</b> - effectindex@gmail.com </li>
+      <li> <b>Bitcoin</b> - 1DvGTSLskP7eYB4t1Mi7KvV56rPEmSg9WT </li>
     </ul>
   </div>
 </template>
@@ -29,4 +29,3 @@ export default {
   }
 };
 </script>
-
