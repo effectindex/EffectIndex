b99011f87e9102c1d87fb4991c675b7d0da90500
Viscid <viscid@gmail.com>
2018-08-07 00:30:39 -0400

    Autoplay fix second attempt

--- components/LightBox/LightBox.vue
+++ components/LightBox/LightBox.vue
@@ -35,6 +35,7 @@
               width="100%"
               height="100%"
               style="position:absolute;top:0;left:0"
+              allow="autoplay"
               allowfullscreen
             />
             
