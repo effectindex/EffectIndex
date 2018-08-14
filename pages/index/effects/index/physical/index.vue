645b1a585fee84968a5589d48da6d20e607bd420
Gabriel Morris <gabemorris@outlook.com>
2018-08-13 18:03:50 -0700

    Bold overviews and change wording

--- pages/index/effects/index/physical/index.vue
+++ pages/index/effects/index/physical/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <i class="fa fa-child categoryIcon" />
-    <p> Physical effects are subjective effects which directly affect part of a person's physical body. </p>
+    <p> <strong>Physical effects</strong> are subjective experiences which directly affect part of a person's physical body. </p>
     <p> This page lists the physical effects which can occur under the influence of psychoactive compounds. It also further organizes these effects into subcategories based on their features and behavior. </p>
     <effect-list list-type="physical" />
   </div>
