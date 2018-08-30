9fd16b17259e4f0f92bc9bc5ca1f3999dd5e2731
Viscid <viscid@gmail.com>
2018-08-30 00:36:38 -0400

    Fix bug???

--- server/models/search/index.js
+++ server/models/search/index.js
@@ -5,40 +5,38 @@ const Report = require('../reports/Report');
 
 router.post("/", async (req, res, next) => {
   const query = req.body.query;
-  if (!query.length) {
-    res.sendStatus(204);
-    return;
+  if (!query) res.sendStatus(204);
+  else {
+    const effectResults = await searchEffects(query)
+      .catch((err) => next(err));
+
+    const reportResults = await searchReports(query)
+      .catch((err) => next(err));
+
+    let results = [];
+
+    effectResults.forEach((result) => {
+      results.push(
+        {
+          _id: result._id,
+          type: 'effect',
+          data: result
+        }
+      );
+    });
+
+    reportResults.forEach((result) => {
+      results.push(
+        {
+          _id: result._id,
+          type: 'report',
+          data: result
+        }
+      );
+    });
+
+    res.send(results);
   }
-
-  const effectResults = await searchEffects(query)
-    .catch((err) => next(err));
-
-  const reportResults = await searchReports(query)
-    .catch((err) => next(err));
-
-  let results = [];
-
-  effectResults.forEach((result) => {
-    results.push(
-      {
-        _id: result._id,
-        type: 'effect',
-        data: result
-      }
-    );
-  });
-
-  reportResults.forEach((result) => {
-    results.push(
-      {
-        _id: result._id,
-        type: 'report',
-        data: result
-      }
-    );
-  });
-
-  res.send(results);
 });
 
 async function searchEffects(query) {
