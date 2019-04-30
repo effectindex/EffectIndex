e4823ebd806f4e855a549d95ca5116959864ebc0
Josie Kins <effectindex@gmail.com>
2019-04-30 14:36:13 -0600

    updated discord description and added image

--- pages/index/discord-chat.vue
+++ pages/index/discord-chat.vue
@@ -18,7 +18,10 @@
     <p>The official Effect Index community is hosted on Discord and may be joined <ext-link href="https://discord.gg/VSm5bF7">here</ext-link>.
     </p>
 
-    <p> This Discord community is used for the discussion of formalised Subjective Effect Documentation and replications of hallucinogenic experiences and effects.
+    <p> This a semi-private community which is not primarily for general conversation but instead aims to be a place of discussion and co-operation for progressing the field of formalised subjective effect documentation. For this reason, we tend to only invite people who are genuinely wanting to contribute to the project. This can include anyone from proofreaders, trip reporters, web developers, replicators, artists, and more.
+    </p>
+
+    <p> If you have an interest in contributing to and taking part in our communities various projects, please pm @josikinz#1066 and tell her why you are interested in this project. Thanks! 
     </p>
 
     <h3> Rules </h3>
@@ -27,7 +30,11 @@
       <li>Please be polite and reasonable.</li>
       <li>You may not discuss any drug sources.</li>
       <li>Keep specific topics within their relevant channels.</li>
-    </ul>
+    </ul>  
+    <img
+      height="350px"
+      class="left"
+      src="/indy_iphone edited.png">  
   </div>
 </template>
 
