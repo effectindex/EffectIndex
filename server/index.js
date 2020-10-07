b64fcdf826f57beb7dd57576f19fb55d7987a341
Viscid <viscid@gmail.com>
2020-10-07 19:57:07 -0400

    Moved chalk logo

--- server/index.js
+++ server/index.js
@@ -68,22 +68,4 @@ async function start() {
 
 start();
 
-let logo = chalk.grey(`
-                   ddddhyyyyyhdddd
-              ddhs+:.\`         \`.:+shdd
-            ddy/-\`    ${chalk.red(`.:+syyys+:\``)}     ./shd
-          dy/.      ${chalk.keyword("orange")(`-ohhs+/:/ohddo.`)}      \`:sdd
-        ds-\`       ${chalk.keyword("yellow")(`:hd+.    `) +
-          chalk.white(`-yd`) +
-          chalk.keyword("yellow")(` ddh:`)}        .+hd
-      ds-         ${chalk.green(`.sd+\`     `) +
-        chalk.white(`.+ys/`) +
-        chalk.green(`+ds.`)}         .od
-      y-          ${chalk.blue(`:yd:           /dy-`)}          -y
-      h/.        ${chalk.keyword("indigo")(`.sdo.         .ods.`)}        \`:y
-        dh+.       ${chalk.keyword("violet")(`-sdy:\`     .:yds-`)}       \`:yd
-          hs:.     ${chalk.red(`\`/shhysosyhhs:\``)}     \`-ohd
-            dhs+-\`    ${chalk.keyword("orange")(`.-/+++/-.`)}    \`-/shd
-                dhyo+:-.......-:+oyhd
-                      dddhhhddd
-                      `);
+let logo = chalk.grey(require('./logo'));
