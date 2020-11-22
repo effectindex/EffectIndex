2145011560996b855ce87f48604855b1300aa95e
Josie Kins <effectindex@gmail.com>
2020-11-22 16:12:13 -0700

    Fixed broken geometry section within psy summary

--- pages/summaries/psychedelics/visual.vue
+++ pages/summaries/psychedelics/visual.vue
@@ -61,17 +61,17 @@
       </div>
 
       <div class="effectsContainer">
-        <h3 style="margin-bottom: 0.25em;">
-          Geometry
-        </h3> 
+        <h3> Geometric patterns </h3>
+        <p class="actionDescription">
+          <b> Geometric patterns </b> are defined as any subjective effect that introduces complex arrays of shapes, colours, symbols, patterns, geometry, form constants, and fractals to one's field of vision.
+        </p>
         <long-summary 
-          v-for="(effect, i) in filterEffectsByTag('psychedelic', 'geometry')"
+          v-for="(effect, i) in filterEffectsByTag('psychedelic', 'visual', 'geometric')"
           :key="effect._id"
-          :show-title="false"
           :index="i"
           :effect="effect"
         />
-      </div>      
+      </div>     
 
       <div class="effectsContainer">
         <h3> Hallucinatory States </h3>
