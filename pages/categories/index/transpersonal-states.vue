82941768c093b32e6b9232dd8469ae2d69843acf
Josie Kins <effectindex@gmail.com>
2020-11-26 23:57:03 -0700

    tweaks to transpersonal category definition

--- pages/categories/index/transpersonal-states.vue
+++ pages/categories/index/transpersonal-states.vue
@@ -12,7 +12,7 @@
     </h1>
 
     <p>
-      <b>Transpersonal states</b> are defined as any subjective effect that feels as if it alters a person's cognition in a manner that relates to their place in the universe, the inner workings of reality or consciousness, and/or the context of their existence. The highest manifestations of these effects fall under what are commonly known as "peak", "transcendent" or "transformative" experiences.
+      <b>Transpersonal effects</b> are defined as any subjective state that feels as if it alters a person's cognition in a manner that relates to their place in the universe, the inner workings of reality or consciousness, and/or the context of their existence. The highest manifestations of these effects fall under what are commonly known as "peak", "transcendent", "mystical", or "transformative" experiences.
     </p>
 
     <p>
@@ -20,7 +20,7 @@
         psychedelic
       </nuxt-link> or <nuxt-link to="/effects/dissociative-intensity-scale">
         dissociative
-      </nuxt-link> experiences. They can occur regardless of the person's spiritual or religious beliefs and often have a distinct and lasting impact on the user's perspective of the world around them. During the experience of a substance-induced transpersonal state, the information conveyed is often felt to be a real and objective truth. However, the person will often come to disagree with these supposed "epiphanies" once the effects of the substance have worn off.
+      </nuxt-link> experiences. They can occur regardless of the person's spiritual or religious beliefs and often have a distinct and lasting impact on the user's perspective of the world around them. During the experience of a substance-induced transpersonal state, the information conveyed is often felt to be a real and objective truth. However, the person will sometimes come to disagree with these supposed "epiphanies" once the effects of the substance have worn off.
     </p>
 
     <p>
@@ -31,7 +31,7 @@
 
     <p>
       It should also be noted that these mind-states are the least reproducible of all effects within the <nuxt-link to="/effects/">
-        subjective effect index.
+        Subjective Effect Index.
       </nuxt-link> Simply taking more of a given substance does not necessarily increase the chances of having these states occur. Instead, they seem to rely more on contextual factors, such as the person's set and setting.
     </p>
     
