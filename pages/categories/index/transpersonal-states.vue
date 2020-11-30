c1c48c157ea6405f28e43ae217e1a25c11d61e39
Josie Kins <effectindex@gmail.com>
2020-11-30 12:47:09 -0700

    fixed transpersonal state vs transpersonal effect

--- pages/categories/index/transpersonal-states.vue
+++ pages/categories/index/transpersonal-states.vue
@@ -12,7 +12,7 @@
     </h1>
 
     <p>
-      <b>Transpersonal effects</b> are defined as any subjective state that feels as if it alters a person's cognition in a manner that relates to their place in the universe, the inner workings of reality or consciousness, and/or the context of their existence. The highest manifestations of these effects fall under what are commonly known as "peak", "transcendent", "mystical", or "transformative" experiences.
+      <b>Transpersonal states</b> are defined as any subjective state that feels as if it alters a person's cognition in a manner that relates to their place in the universe, the inner workings of reality or consciousness, and/or the context of their existence. The highest manifestations of these effects fall under what are commonly known as "peak", "transcendent", "mystical", or "transformative" experiences.
     </p>
 
     <p>
