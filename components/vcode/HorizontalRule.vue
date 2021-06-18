cf7e9e3a67b324b49292feed7a259ab2e849ce3b
liv <~@l1v.in>
2021-06-18 03:30:36 +0000

    new: Add horizontalRuleTransparent tag (#23)
    * new: Add horizontalRuleTransparent tag
    
    * impr: Switch HorizontalRule to vcode
    
    * fix: Add missing `HorizontalRule.vue` that I forgot to commit
    
    * fix: Improper import

---
+++ components/vcode/HorizontalRule.vue
@@ -0,0 +1,28 @@
+<template>
+  <hr
+    :style="`opacity: ${opacity}`"
+  >
+</template>
+
+<script>
+
+export default {
+    props: {
+        opacity: {
+            type: String,
+            default: "1.0"
+        }
+    }
+};
+
+</script>
+
+<style scoped>
+
+hr {
+  margin: 50px 0;
+  border: 0;
+  border-top: 1px solid #ccc;
+}
+
+</style>
