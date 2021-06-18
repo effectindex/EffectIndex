8e0889f999abc2bf65c8d14be1a8085692349bdb
lv <~@l1v.in>
2021-06-17 23:04:30 -0400

    fix: Add missing `HorizontalRule.vue` that I forgot to commit

---
+++ components/vcode/HorizontalRule.vue
@@ -0,0 +1,28 @@
+<template>
+  <hr
+    :style="`opacity: ${hrOpacity}`"
+  >
+</template>
+
+<script>
+
+export default {
+    props: {
+        hrOpacity: {
+            type: String
+            default: "1.0"
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+hr {
+  margin: 50px 0;
+  border: 0;
+  border-top: 1px solid #ccc;
+}
+
+</style>
