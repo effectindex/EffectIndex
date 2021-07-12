fb364d43e2f3b4c9bf03bc55e80f1fbacb7149c4
Viscid <viscid@gmail.com>
2021-07-12 19:40:06 -0400

    Fixes redirects

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,13 +1,12 @@
 // Fetches and stores redirects, redirects if redirect matches current route.
 export default async function({ route, redirect, store }) {
   try {
-    const { list } = store.state.redirects.list ? store.state.redirects : await store.dispatch('redirects/get');
-    if (Array.isArray(list)) {
-      const foundRedirect = list.find( r => r.from === route.fullPath.slice(1));
-      if (foundRedirect && (route.fullPath !== ('/' + foundRedirect.to))) {
-        redirect('/' + foundRedirect.to);
-      }
+    if (!store.state.redirects.fetched) await store.dispatch('redirects/get');
+    const foundRedirect = store.state.redirects.list.find( r => r.from === route.fullPath.slice(1));
+    if (foundRedirect && (route.fullPath !== ('/' + foundRedirect.to))) {
+      redirect('/' + foundRedirect.to);
     }
+  
   } catch (error) {
     console.log('Redirect middleware error:');
     console.log(error);
