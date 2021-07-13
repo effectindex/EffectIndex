465171c0c750d1232a6f7a4fd62ff3139e220dec
Viscid <viscid@gmail.com>
2021-07-13 14:58:48 -0400

    Reverts panel changes

--- components/vcode/components/Panel.vue
+++ components/vcode/components/Panel.vue
@@ -21,7 +21,6 @@
 
 <script>
 import Icon from '@/components/Icon';
-
 export default {
   components: {
     Icon
@@ -49,15 +48,12 @@ export default {
     width: 24px;
     opacity: 0.75;
   }
-
   .panel {
     border: 1px solid #DDD;
   }
-
   .panel:not(:last-child) {
     margin-bottom: 10px;
   }
-
   .panel-header {
     display: flex;
     flex-direction: row;
@@ -70,61 +66,44 @@ export default {
     border-bottom: 1px solid #DDD;
     letter-spacing: 1px;
   }
-
   .panel-header h4 {
     margin: 0;
   }
-
   .panel-header > h4 {
     font-family: "Proxima Nova",-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
     font-size: 20px; 
     color: #3d9991;
     letter-spacing: 1px;
   }
-
   .panel-content > * {
     margin: 0;
   }
-
   .panel-content {
     margin: 0;
-    padding: 0.5em 1em;
   }
-
-  .panel-content >>> p:not(:last-child) {
-    margin-bottom: 1em;
-  }
-
   .panel-content >>> ul {
     margin: 0;
     padding-left: 0;
-    margin-left: 1em;
   }
-
-  .panel-content >>> ul h4 {
+  .panel-content >>> h4 {
     font-size: 20px;
     margin: 0;
     padding: 0.5em 0 0 0.8em;
     text-transform: uppercase;
   }
-
-  .panel-content >>> ul h4:not(:first-child) {
+  .panel-content >>> h4:not(:first-child) {
     border-top: 1px solid #ddd;
   }
-
   .panel-content >>> ul ul {
     margin: 0;
   }
-
   .panel-content >>> ul li {
-    margin: 0.5em 0 0.5em 0em;
+    margin: 0.5em 0 0.5em 2em;
     font-weight: 300;
   }
-
   .panel-content >>> ul li::marker {
     color: #ddd;
   }
-
   .panel-content >>> h1, .panel-content >>> h2, .panel-content >>> h3, .panel-content >>> h4 {
     margin: 0.5em 0;
   }
