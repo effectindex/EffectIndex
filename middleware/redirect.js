d2f442162fcf12af9c0bc6517e168df152f83adc
5HT2 <git@frogg.ie>
2022-05-18 16:56:36 -0400

    impr: Make redirects ignore trailing slashes

--- middleware/redirect.js
+++ middleware/redirect.js
@@ -1,12 +1,11 @@
 // Fetches and stores redirects, redirects if redirect matches current route.
-export default async function({ route, redirect, store }) {
+export default async function ({route, redirect, store}) {
   try {
     if (!store.state.redirects.fetched) await store.dispatch('redirects/get');
-    const foundRedirect = store.state.redirects.list.find( r => r.from === route.fullPath.slice(1));
+    const foundRedirect = store.state.redirects.list.find(r => r.from === route.fullPath.slice(1) || r.from + "/" === route.fullPath.slice(1));
     if (foundRedirect && (route.fullPath !== ('/' + foundRedirect.to))) {
       redirect('/' + foundRedirect.to);
     }
-  
   } catch (error) {
     console.log('Redirect middleware error:');
     console.log(error);
